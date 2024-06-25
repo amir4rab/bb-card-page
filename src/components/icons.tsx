69a250b9fa4a7a48046730cd342e36987b6c4d70
Amir <60965542+amir4rab@users.noreply.github.com>
2024-06-25 07:26:25 -0400

    🗂️ Added icons placeholder

---
+++ src/components/icons.tsx
@@ -0,0 +1,96 @@
+/** Due to licensing concerns, the original icons aren't present at this code, and they are replaced with this place holder */
+const PlaceHolder = () => {
+  return (
+    <>
+      <path d="M19.7321 19.4405C19.9443 19.5331 20.1935 19.463 20.3169 19.2671C21.2569 17.7748 21.8002 16.0114 21.8002 14.122C21.8002 8.7527 17.4126 4.4 12.0002 4.4C6.5878 4.4 2.2002 8.7527 2.2002 14.122C2.2002 16.0114 2.74345 17.7748 3.68345 19.267C3.80687 19.463 4.0561 19.5331 4.26833 19.4405V19.4405C4.55074 19.3173 4.64677 18.965 4.48428 18.7032C3.65725 17.3707 3.1802 15.8017 3.1802 14.122C3.1802 9.28963 7.12904 5.3722 12.0002 5.3722C16.8713 5.3722 20.8202 9.28963 20.8202 14.122C20.8202 15.8017 20.3431 17.3708 19.5161 18.7032C19.3536 18.965 19.4496 19.3173 19.7321 19.4405V19.4405Z" />
+      <path d="M17.9183 18.649C18.1213 18.7376 18.3598 18.6767 18.4851 18.4941C19.3403 17.2482 19.8402 15.743 19.8402 14.122C19.8402 9.82657 16.3301 6.34441 12.0002 6.34441C7.67028 6.34441 4.1602 9.82657 4.1602 14.122C4.1602 15.743 4.66007 17.2482 5.51524 18.4941C5.64056 18.6767 5.87911 18.7376 6.08207 18.649V18.649C6.37324 18.5219 6.4625 18.1519 6.28549 17.8881C5.56188 16.8097 5.1402 15.5147 5.1402 14.122C5.1402 10.3635 8.21152 7.31661 12.0002 7.31661C15.7889 7.31661 18.8602 10.3635 18.8602 14.122C18.8602 15.5147 18.4385 16.8097 17.7149 17.8881C17.5379 18.1519 17.6271 18.522 17.9183 18.649V18.649Z" />
+      <path d="M16.0906 17.8514C16.2773 17.9329 16.4967 17.8864 16.6233 17.7269C17.4106 16.7346 17.8802 15.4827 17.8802 14.122C17.8802 10.9004 15.2476 8.28881 12.0002 8.28881C8.75276 8.28881 6.1202 10.9004 6.1202 14.122C6.1202 15.4827 6.58983 16.7346 7.37708 17.7269C7.50367 17.8864 7.72309 17.9329 7.90977 17.8514V17.8514C8.21567 17.7179 8.29055 17.3153 8.08831 17.0498C7.46805 16.2355 7.1002 15.2214 7.1002 14.122C7.1002 11.4374 9.294 9.26101 12.0002 9.26101C14.7064 9.26101 16.9002 11.4374 16.9002 14.122C16.9002 15.2214 16.5323 16.2355 15.9121 17.0498C15.7098 17.3153 15.7847 17.718 16.0906 17.8514V17.8514Z" />
+      <path d="M14.2266 17.038C14.3752 17.1028 14.5488 17.0812 14.6679 16.9714C15.4383 16.2613 15.9202 15.2475 15.9202 14.122C15.9202 11.9743 14.1652 10.2332 12.0002 10.2332C9.83524 10.2332 8.0802 11.9743 8.0802 14.122C8.0802 15.2475 8.56213 16.2613 9.33244 16.9714C9.45159 17.0812 9.62523 17.1028 9.77375 17.038V17.038C10.1058 16.8931 10.1355 16.4056 9.88305 16.1457C9.37354 15.6213 9.0602 14.9079 9.0602 14.122C9.0602 12.5112 10.3765 11.2054 12.0002 11.2054C13.6239 11.2054 14.9402 12.5112 14.9402 14.122C14.9402 14.9079 14.6268 15.6213 14.1173 16.1457C13.8649 16.4056 13.8946 16.8931 14.2266 17.038V17.038Z" />
+      <path d="M13.4702 14.122C13.4702 14.9274 12.8121 15.5803 12.0002 15.5803C11.1883 15.5803 10.5302 14.9274 10.5302 14.122C10.5302 13.3166 11.1883 12.6637 12.0002 12.6637C12.8121 12.6637 13.4702 13.3166 13.4702 14.122Z" />
+    </>
+  );
+};
+
+/** "Share" ICON FROM GOOGLE FONTS - Weight 700, Grade 0, Optical Size 48px, Style Rounded */
+export const ShareIcon = (props: { class?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class={props.class ? props.class : "size-6"}
+    >
+      <PlaceHolder />
+    </svg>
+  );
+};
+
+/** "Pause" ICON FROM GOOGLE FONTS - Weight 700, Grade 0, Optical Size 48px, Style Rounded */
+export const PauseIcon = (props: { class?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class={props.class ? props.class : "size-6"}
+    >
+      <PlaceHolder />
+    </svg>
+  );
+};
+
+/** "Lock Reset" ICON FROM GOOGLE FONTS - Weight 700, Grade 0, Optical Size 48px, Style Rounded */
+export const LockResetIcon = (props: { class?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class={props.class ? props.class : "size-6"}
+    >
+      <PlaceHolder />
+    </svg>
+  );
+};
+
+/** "Pin" ICON FROM GOOGLE FONTS - Weight 700, Grade 0, Optical Size 48px, Style Rounded */
+export const PinIcon = (props: { class?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class={props.class ? props.class : "size-6"}
+    >
+      <PlaceHolder />
+    </svg>
+  );
+};
+
+/** "Share" ICON FROM GOOGLE FONTS - Weight 700, Grade 0, Optical Size 48px, Style Rounded */
+export const LightBulbIcon = (props: { class?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class={props.class ? props.class : "size-6"}
+    >
+      <PlaceHolder />
+    </svg>
+  );
+};
+
+/** "Arrow Forward iOS" ICON FROM GOOGLE FONTS - Weight 700, Grade 0, Optical Size 48px, Style Rounded */
+export const ArrowForwardIcon = (props: { class?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class={props.class ? props.class : "size-6"}
+    >
+      <PlaceHolder />
+    </svg>
+  );
+};

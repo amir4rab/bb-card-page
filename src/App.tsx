2b1fe3a9fb3dc2d8fcd77b2eb4fff21cb9e0bf67
Amir <60965542+amir4rab@users.noreply.github.com>
2024-06-25 08:36:41 -0400

    🗂️ Added the components into the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { createSignal } from "solid-js";
+
+// Components
+import Card from "./components/card";
+import CardMenu from "./components/card-menu";
+import CardActions from "./components/card-actions";
+
 function App() {
+  const [focused, setFocused] = createSignal(false);
+
   return (
-    <div>
-      <h1 class="text-2xl">Hello world</h1>
+    <div
+      data-focused={focused()}
+      class="min-h-svh data-[focused=true]:overflow-clip data-[focused=true]:max-h-svh"
+    >
+      <div class="flex justify-center items-center h-48 sticky top-0">
+        <Card focused={focused} setFocused={setFocused} />
+      </div>
+      <CardActions focused={focused} />
+      <CardMenu focused={focused} />
     </div>
   );
 }
